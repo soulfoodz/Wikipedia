3223cb361b16df029b0b3822a8e3941280dae893
Eric Webb <ericwebb85@yahoo.com>
2013-10-03 14:51:34 -0500

    first real commit

---
+++ Wikipedia/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wikipedia
+//
+//  Created by MacBook Pro on 10/3/13.
+//  Copyright (c) 2013 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wikipedia/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Wikipedia
+//
+//  Created by MacBook Pro on 10/3/13.
+//  Copyright (c) 2013 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
